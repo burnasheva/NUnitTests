9df430b13ba7dba9fc3ebba4a4f15ab282257d6a
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2016-01-22 15:38:19 +0300

    add new class

---
+++ NUnit300/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NUnit300
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        [Category("calc"), Category("smth")]
+        public void TestPlus()
+        {
+            Assert.That(1 + 3, Is.EqualTo(4));
+        }
+
+        [Test]
+        [Category("calc")]
+        public void TestMinus()
+        {
+            Assert.That(1 - 3, Is.EqualTo(-2));
+        }
+
+        [Test]
+        [Category("calc")]
+        public void TestMultiply()
+        {
+            Assert.That(1 * 3, Is.EqualTo(3));
+        }
+
+        [Test]
+        [Category("calc")]
+        public void TestDivizion()
+        {
+            Assert.That(3 / 3, Is.EqualTo(1));
+        }
+
+        [Test]
+        [Category("smth")]
+        public void TestSmth()
+        {
+        }
+    }
+}
