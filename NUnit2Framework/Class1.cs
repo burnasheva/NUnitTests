d389656b4083d96005fe53d39263f380e380d97d
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2016-01-27 15:01:42 +0300

    add test project with .net framwork 2.0

---
+++ NUnit2Framework/Class1.cs
@@ -0,0 +1,42 @@
+﻿namespace NUnit2Framework
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        [Category("calc"), Category("smth")]
+        public void TestPlus()
+        {
+            Assert.That(1 + 3, Is.EqualTo(4));
+        }
+
+        [Test]
+        [Category("calc")]
+        public void TestMinus()
+        {
+            Assert.That(1 - 3, Is.EqualTo(-2));
+        }
+
+        [Test]
+        [Category("calc")]
+        public void TestMultiply()
+        {
+            Assert.That(1 * 3, Is.EqualTo(3));
+        }
+
+        [Test]
+        [Category("calc")]
+        public void TestDivizion()
+        {
+            Assert.That(3 / 3, Is.EqualTo(1));
+        }
+
+        [Test]
+        [Category("smth")]
+        public void TestSmth()
+        {
+        }
+    }
+}
