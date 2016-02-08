ff87a4b999bffb16a1dcca6a62de4524193636a1
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2016-02-08 13:15:04 +0300

    add new not covered class

---
+++ NUnit301/ClassToTest.cs
@@ -0,0 +1,15 @@
+﻿namespace NUnit301
+{
+    public class ClassToTest
+    {
+        public double Plus(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Minus(double a, double b)
+        {
+            return a - b;
+        }
+    }
+}
