45dc950454250c61f73ce1974b76bea559ff0ea3
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2016-02-08 13:33:51 +0300

    add another class

---
+++ NUnit301/ClassToExcludeFromCoverage.cs
@@ -0,0 +1,25 @@
+﻿namespace NUnit301
+{
+    public class ClassToExcludeFromCoverage
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Minus(double a, double b)
+        {
+            return a - b; 
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a*b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            return a/b;
+        }
+    }
+}
