2b061074e3c7b5825c9d255575cb62b062e750bd
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2016-03-09 16:03:20 +0300

    add nunit project with version 3.2.0

---
+++ NUnit320/SomeTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace NUnit320
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SomeTests
+    {
+        [Test]
+        [Category("calc"), Category("smth")]
+        public void TestPlus()
+        {
+            Assert.That(1 + 3, Is.EqualTo(4));
+        }
+
+        [Test]
+        [Category("calc")]
+        public void TestMinus()
+        {
+            Assert.That(1 - 3, Is.EqualTo(-2));
+        }
+
+        [Test]
+        [Category("calc")]
+        public void TestMultiply()
+        {
+            Assert.That(1 * 3, Is.EqualTo(3));
+        }
+
+        [Test]
+        [Category("calc")]
+        public void TestDivizion()
+        {
+            Assert.That(3 / 3, Is.EqualTo(1));
+        }
+
+        [Test]
+        [Category("smth")]
+        public void TestSmth()
+        {
+        }
+    }
+}
