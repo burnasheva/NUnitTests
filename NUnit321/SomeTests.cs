e3bc4596653923f3fac2cf5bebdc69b6bc07eb5b
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2016-04-27 16:52:24 +0300

    add project for nunit 3.2.1

---
+++ NUnit321/SomeTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace NUnit321
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SomeTests
+    {
+        [Test]
+        [Category("calc"), Category("smth")]
+        public void TestPlus()
+        {
+            Console.WriteLine("http://user:123456@jetbrains.com");
+            Console.WriteLine("123456");
+            Assert.That(1 + 3, Is.EqualTo(5), "123456 password");
+        }
+
+
+        [Test]
+        [Category("calc")]
+        public void TestMinus()
+        {
+            Assert.That(1 - 3, Is.EqualTo(-3), "http://user:123456@jetbrains.com");
+        }
+
+        [Test]
+        [Category("calc")]
+        public void TestMultiply()
+        {
+            Assert.That(1 * 3, Is.EqualTo(2), "123456");
+        }
+
+        [Test]
+        [Ignore("http://user:123456@jetbrains.com")]
+        [Category("calc")]
+        public void TestDivizion()
+        {
+            Assert.That(3 / 3, Is.EqualTo(1));
+        }
+
+        [Test]
+        [Category("smth")]
+        public void TestSmth()
+        {
+        }
+    }
+}
