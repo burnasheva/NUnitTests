34e3f8d7e0477366d17422efce365bdb4314ce25
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2016-05-30 13:10:03 +0300

    add project with 3.5 framework

---
+++ NUnit32x_35/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NUnit32x_35
+{
+    public class Class1
+    {
+    }
+}
---
+++ NUnit32x_35/SomeTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace NUnit32x
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SomeTests
+    {
+        [Test]
+        [Repeat(5)]
+        [Category("calc"), Category("smth")]
+        public void TestPlus()
+        {
+            Console.WriteLine("http://user:123456@jetbrains.com");
+            Console.WriteLine("123456");
+            Assert.That(1 + 3, Is.EqualTo(5), "123456 password");
+        }
+
+
+        [Test]
+        [Category("calc")]
+        public void TestMinus()
+        {
+            Assert.That(1 - 3, Is.EqualTo(-3), "http://user:123456@jetbrains.com");
+        }
+
+        [Test]
+        [Repeat(7)]
+        [Category("calc")]
+        public void TestMultiply()
+        {
+            Assert.That(1 * 3, Is.EqualTo(3), "123456");
+        }
+
+        [Test]
+        [Ignore("http://user:123456@jetbrains.com")]
+        [Category("calc")]
+        public void TestDivizion()
+        {
+            Assert.That(3 / 3, Is.EqualTo(1));
+        }
+
+        [Test]
+        [Category("smth")]
+        public void TestSmth()
+        {
+        }
+    }
+}
