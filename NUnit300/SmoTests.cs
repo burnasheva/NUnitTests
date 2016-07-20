1ed69d3693299e9d41019c9607875dae22745b1d
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2016-07-20 14:00:31 +0300

    add package with smo

---
+++ NUnit300/SmoTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.SqlServer.Management.Smo;
+
+namespace NUnit300
+{
+    using NUnit.Framework;
+    
+    [TestFixture]
+    public class SmoTests
+    {
+        [Test]
+        [Category("calc"), Category("smth")]
+        public void TestPlus()
+        {
+            try
+            {
+                var server = new Server(String.Empty);
+                server.ConnectionContext.ExecuteNonQuery(String.Empty);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Успех - моё второе имя");
+            }
+            Assert.True(true);
+        }
+
+    }
+}
