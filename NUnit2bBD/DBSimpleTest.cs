b577a6c717e0043076536ef087b2ee6f575be374
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2016-07-25 15:45:35 +0300

    add new project with tests on db

---
+++ NUnit2bBD/DBSimpleTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using NUnit.Framework;
+
+namespace NUnit2bBD
+{
+    public class DBSimpleTest
+    {
+        [Test]
+        public void GetVersionTest()
+        {
+            String sqlServerLogin = "jetbrains";
+            String password = "jetbrains";
+            String instanceName = "";
+            String remoteSvrName = "tcqa-mssql-2014.labs.intellij.net";
+
+            ServerConnection srvConn = new ServerConnection();
+            srvConn.ServerInstance = remoteSvrName + "\\" + instanceName;
+            srvConn.LoginSecure = false;
+            srvConn.Login = sqlServerLogin;
+            srvConn.Password = password;
+            Server srv = new Server(srvConn);
+
+            Console.WriteLine(srv.Information.Version);
+
+            Assert.AreEqual("12.0.4100", srv.Information.Version.ToString());
+        }
+    }
+}
