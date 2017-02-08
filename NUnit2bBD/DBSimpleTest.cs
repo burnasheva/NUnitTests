004ad12c10200fd6fda2d429ee244f7409b97fb2
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2017-02-08 11:29:55 +0100

    Update DBSimpleTest.cs

--- NUnit2bBD/DBSimpleTest.cs
+++ NUnit2bBD/DBSimpleTest.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Microsoft.SqlServer.Management.Common;
-using Microsoft.SqlServer.Management.Smo;
+using System;
 using NUnit.Framework;
 
 namespace NUnit2bBD
@@ -10,21 +8,21 @@ namespace NUnit2bBD
         [Test]
         public void GetVersionTest()
         {
-            String sqlServerLogin = "jetbrains";
-            String password = "jetbrains";
-            String instanceName = "";
-            String remoteSvrName = "tcqa-mssql-2014.labs.intellij.net";
+//             String sqlServerLogin = "jetbrains";
+//             String password = "jetbrains";
+//             String instanceName = "";
+//             String remoteSvrName = "tcqa-mssql-2014.labs.intellij.net";
 
-            ServerConnection srvConn = new ServerConnection();
-            srvConn.ServerInstance = remoteSvrName + "\\" + instanceName;
-            srvConn.LoginSecure = false;
-            srvConn.Login = sqlServerLogin;
-            srvConn.Password = password;
-            Server srv = new Server(srvConn);
+//             ServerConnection srvConn = new ServerConnection();
+//             srvConn.ServerInstance = remoteSvrName + "\\" + instanceName;
+//             srvConn.LoginSecure = false;
+//             srvConn.Login = sqlServerLogin;
+//             srvConn.Password = password;
+//             Server srv = new Server(srvConn);
 
-            Console.WriteLine(srv.Information.Version);
+//             Console.WriteLine(srv.Information.Version);
 
-            Assert.AreEqual("12.0.4100", srv.Information.Version.ToString());
+//             Assert.AreEqual("12.0.4100", srv.Information.Version.ToString());
         }
     }
 }
