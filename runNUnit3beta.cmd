2db95e95ab79182b4b8cece9206a0b8dcb4a5cc7
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2017-05-22 12:22:26 +0200

    Update runNUnit3beta.cmd

--- runNUnit3beta.cmd
+++ runNUnit3beta.cmd
@@ -1,1 +1,1 @@
-packages\NUnit.Console.3.0.0-beta-%1\tools\nunit-console.exe NUnit3beta%1\bin\Debug\NUnit3beta%1.dll --workers=1 --teamcity --noresult
+packages\NUnit.Console.3.0.0-beta-%1\tools\nunit-console.exe NUnit3beta%1\bin\Debug\NUnit3beta%1.dll  --workers=1 --teamcity --noresult
