8593f8eb8fb93acf3a054916fc99d8e06c7debf5
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2018-03-23 17:40:05 +0100

    Update UnitTest1.cs

--- NUnit2/UnitTest1.cs
+++ NUnit2/UnitTest1.cs
@@ -21,7 +21,7 @@ namespace NUnit2
         {
             Console.WriteLine("http://user:123456@jetbrains.com");
             Console.WriteLine("123456");
-            Assert.That(1 + 3, Is.EqualTo(4), "123456 password");
+            Assert.That(1 + 2, Is.EqualTo(4), "123456 password");
         }
 
 
